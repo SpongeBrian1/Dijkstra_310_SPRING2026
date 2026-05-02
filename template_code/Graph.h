fb10b89141e026a49e1299a584cb52bf48c1cb42
SpongeBrian1 <brianrockz49@gmail.com>
2026-05-02 00:50:55 -0700

    I think I did it. time to test the code

--- template_code/Graph.h
+++ template_code/Graph.h
@@ -4,5 +4,29 @@
 #include "Vertex.h"
 #include "Edge.h"
 #include <climits>
+#define MAX_V 2100
+
+class Graph{
+private:
+    int adjMatrix[MAX_V][MAX_V]; // This is the adjacency matrix
+    Vertex vertices[MAX_V]; //vertex array
+    int n; //num of vertices
+    int m; //num of edges
+    int minDistVertex(int dist[], bool visited[]);
+
+public:
+    Graph();
+
+    void loadGraph(int numVerts, int numEdges, Edge* edges, int edgeCount);
+
+    void printAdjMatrix();
+
+    int getOddDegreeNodes(int oddNodesp[]);
+
+    void printOddDegreeNodes(int oddNodes[], int count);
+
+    void dijkstra(int source);
+}
+
 
 #endif
--- template_code/Main.cpp
+++ template_code/Main.cpp
@@ -5,8 +5,8 @@ int main(){
 
     // start of the initial pipeline that loads the test case file stream from std::cin
 
-    int numOfVertices;
-    int numOfEdges;
+    int numOfVertices = 0;
+    int numOfEdges = 0;
 
     if(!std::cin.eof()){
         std::cin >> numOfVertices;
@@ -27,9 +27,23 @@ int main(){
         Edge* newEdge = new Edge(startVertice, endVertice);
         newEdge->setWeight(weight);
         // Here is where you load up the Graph object
+        Graph* g = new Graph();
+        g->loadGraph(numOfVertices, numOfEdges, edges, edgeCount);
+
+        g->printAdjMatrix();
+
+        int oddNodes[MAX_V];
+        int oddcount = g->getOddDegreeNodes(oddNodes);
+        g->printOddDegreeNodes(oddNodes, oddCount);
+
+        for (int i = 0; i < oddCount; i++){
+            g->dijkstra(oddNodes[i]);
+        }
+
+        delete[] edges;
+        delete g;
     }
 
     // And here is where you start working on the three tasks
-
     return 0;
 }
